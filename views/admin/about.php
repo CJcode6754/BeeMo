febb65da63894ea7167860011540def3cea0b7f3
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-08 11:25:37 +0800

    fix

--- views/admin/about.php
+++ views/admin/about.php
@@ -84,7 +84,7 @@
 
         <div class="col-6">
             <div class="mapswrapper">
-                <iframe width="100%" height="100%" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=pj%20plantation&zoom=10&maptype=roadmap"></iframe>
+                <!-- <iframe width="100%" height="100%" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=pj%20plantation&zoom=10&maptype=roadmap"></iframe> -->
             </div>
         </div>
 
