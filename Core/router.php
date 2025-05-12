750e8d969e58334728e0cdfe78ce0c6bc21f21f1
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-12 19:17:31 +0800

    feat: implement middleware

--- Core/router.php
+++ Core/router.php
@@ -1,35 +1,65 @@
-<?php 
+<?php
+
 namespace Core;
-class Router {
+
+use Core\Middleware\Auth;
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
+
+class Router
+{
     protected $routes = [];
 
-    public function add($method, $uri, $controller){
-        $this->routes[] = compact('method', 'uri', 'controller');
+    public function add($method, $uri, $controller)
+    {
+        $middleware = null;
+        $this->routes[] = compact('method', 'uri', 'controller', 'middleware');
+
+        return $this;
     }
 
-    public function get($uri, $controller){
-        $this->add('GET', $uri, $controller);
-    }
-    
-    public function post($uri, $controller){
-        $this->add('POST', $uri, $controller);
+    public function get($uri, $controller)
+    {
+        return $this->add('GET', $uri, $controller);
     }
 
-    public function delete($uri, $controller){
-        $this->add('DELETE', $uri, $controller);
+    public function post($uri, $controller)
+    {
+        return $this->add('POST', $uri, $controller);
     }
 
-    public function put($uri, $controller){
-        $this->add('PUT', $uri, $controller);
+    public function delete($uri, $controller)
+    {
+        return $this->add('DELETE', $uri, $controller);
     }
 
-    public function patch($uri, $controller){
-        $this->add('PATCH', $uri, $controller);
+    public function put($uri, $controller)
+    {
+        return $this->add('PUT', $uri, $controller);
     }
 
-    public function route($uri, $method){
-        foreach($this->routes as $route){
-            if($route['uri'] === $uri && $route['method'] === strtoupper($method)){
+    public function patch($uri, $controller)
+    {
+        return $this->add('PATCH', $uri, $controller);
+    }
+
+    public function only($key)
+    {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+
+        return $this;
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri && $route['method'] === strtoupper($method)) {
+
+                if ($route['middleware']) {
+                    $middleware = Middleware::MAP[$route['middleware']];
+
+                    (new $middleware)->handle();
+                }
                 return require base_path($route['controller']);
             }
         }
@@ -37,7 +67,8 @@ class Router {
         $this->abort();
     }
 
-    public function abort($code = 404){
+    public function abort($code = 404)
+    {
         http_response_code($code);
 
         require base_path("views/partials/{$code}.php");
--- routes.php
+++ routes.php
@@ -1,18 +1,4 @@
 <?php
-
-// return [
-//     '/' => 'controllers/dashboard.php',
-//     '/chooseHive' => 'controllers/chooseHive.php',
-//     '/parameterMonitoring' => 'controllers/parameterMonitoring.php',
-//     '/reports' => 'controllers/reports.php',
-//     '/harvestCycle' => 'controllers/harvestCycle.php',
-//     '/beeguide' => 'controllers/beeguide.php',
-//     '/workers' => 'controllers/worker/index.php',
-//     '/worker' => 'controllers/worker/show.php',
-//     '/worker/create' => 'controllers/worker/create.php',
-//     '/about' => 'controllers/about.php',
-// ];
-
 //GET METHOD
 $router->get('/', 'controllers/dashboard.php');
 $router->get('/chooseHive','controllers/chooseHive.php');
@@ -24,9 +10,16 @@ $router->get('/about','controllers/about.php');
 $router->get('/workers', 'controllers/worker/index.php');
 $router->get('/worker', 'controllers/worker/show.php');
 $router->get('/worker/create', 'controllers/worker/create.php');
+$router->get('/worker/edit', 'controllers/worker/edit.php');
 
 //POST METHOD
 $router->post('/worker/store', 'controllers/worker/store.php');
 
-//DELET METHOD
+//DELETE METHOD
 $router->delete('/worker/delete', 'controllers/worker/destroy.php');
+
+$router->patch('/worker/patch', 'controllers/worker/update.php');
+
+$router->get('/login', 'controllers/auth/create.php')->only('guest');
+$router->post('/sessions', 'controllers/auth/store.php')->only('guest');
+$router->delete('/session', 'controllers/auth/destroy.php')->only('auth');
