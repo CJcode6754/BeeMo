342e9fc46e7e6e4b0fd38f853577f02397271dca
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-12 19:17:43 +0800

    modified

--- Core/Container.php
+++ Core/Container.php
@@ -2,23 +2,27 @@
 
 namespace Core;
 
+use Exception;
+
 class Container
 {
     protected $bindings = [];
-    public function bind($key, $builder)
+
+    public function bind($key, $resolver)
     {
-        $this->bindings[$key] = $builder;
+        $this->bindings[$key] = $resolver;
     }
 
+    /**
+     * @throws Exception
+     */
     public function resolve($key)
     {
-        if (! array_key_exists($key, $this->bindings)) {
-            throw new \Exception("No matching binding found for {$key}");
+        if (!array_key_exists($key, $this->bindings)) {
+            throw new Exception("No matching binding found for {$key}");
         }
 
-        if (array_key_exists($key, $this->bindings)){
-            $resolver = $this->bindings[$key];
-        };
+        $resolver = $this->bindings[$key];
 
         return call_user_func($resolver);
     }
--- bootstrap.php
+++ bootstrap.php
@@ -4,9 +4,9 @@ use Core\App;
 use Core\Container;
 use Core\Database;
 
-$container = new Container;
+$container = new Container();
 
-$container->bind('Core\Database', function (){
+$container->bind('Core\Database', function () {
     $config = require base_path('config.php');
 
     return new Database($config['database']);
