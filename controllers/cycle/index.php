e4f1c4cd35f0615c4bc2caecd5b8441c27a6ea71
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-13 20:00:33 +0800

    refactor: implement session

--- controllers/cycle/index.php
+++ controllers/cycle/index.php
@@ -4,9 +4,9 @@ use Core\App;
 use Core\Database;
 
 $db = App::resolve(Database::class);
-
-$admin_cycles = $db->query('SELECT * FROM harvest_cycle where admin_id = 10')->get();
-$worker_cycles = $db->query('SELECT * FROM user_harvest_cycle where admin_id = 10')->get();
+$id = $_SESSION['user']['id'];
+$admin_cycles = $db->query("SELECT * FROM harvest_cycle where admin_id = {$id}")->get();
+$worker_cycles = $db->query("SELECT * FROM user_harvest_cycle where admin_id = {$id}")->get();
 view("cycle/index.php", [
     'heading' => 'Harvest Cycle',
     'admin_cycles' => $admin_cycles,
