1f8847a30b9f93d36a5b2d5ff9133d4db2f80241
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-14 08:55:33 +0800

    rafactor: move controllers folder in Http folder

--- routes.php
+++ routes.php
@@ -1,30 +1,30 @@
 <?php
 //GET METHOD
-$router->get('/', 'controllers/dashboard.php');
-$router->get('/chooseHive','controllers/chooseHive.php');
-$router->get('/parameterMonitoring','controllers/parameterMonitoring.php');
-$router->get('/reports','controllers/reports.php');
-$router->get('/beeguide','controllers/beeguide.php');
-$router->get('/about','controllers/about.php');
-$router->get('/workers', 'controllers/worker/index.php');
-$router->get('/worker', 'controllers/worker/show.php');
-$router->get('/worker/create', 'controllers/worker/create.php');
-$router->get('/worker/edit', 'controllers/worker/edit.php');
+$router->get('/', 'dashboard.php');
+$router->get('/chooseHive','chooseHive.php');
+$router->get('/parameterMonitoring','parameterMonitoring.php');
+$router->get('/reports','reports.php');
+$router->get('/beeguide','beeguide.php');
+$router->get('/about','about.php');
+$router->get('/workers', 'worker/index.php');
+$router->get('/worker', 'worker/show.php');
+$router->get('/worker/create', 'worker/create.php');
+$router->get('/worker/edit', 'worker/edit.php');
 
 //POST METHOD
-$router->post('/worker/store', 'controllers/worker/store.php');
+$router->post('/worker/store', 'worker/store.php');
 
 //DELETE METHOD
-$router->delete('/worker/delete', 'controllers/worker/destroy.php');
+$router->delete('/worker/delete', 'worker/destroy.php');
 
-$router->patch('/worker/patch', 'controllers/worker/update.php');
+$router->patch('/worker/patch', 'worker/update.php');
 
-$router->get('/login', 'controllers/auth/create.php')->only('guest');
-$router->post('/sessions', 'controllers/auth/store.php')->only('guest');
-$router->delete('/session', 'controllers/auth/destroy.php')->only('auth');
+$router->get('/login', 'auth/create.php')->only('guest');
+$router->post('/sessions', 'auth/store.php')->only('guest');
+$router->delete('/session', 'auth/destroy.php')->only('auth');
 
-$router->get('/harvestCycle', 'controllers/cycle/index.php');
-$router->post('/harvestCycle/create', 'controllers/cycle/create.php');
-$router->get('/harvestCycle/edit', 'controllers/cycle/edit.php');
-$router->patch('/harvestCycle/patch', 'controllers/cycle/update.php');
-$router->delete('/harvestCycle/delete', 'controllers/cycle/destroy.php');
+$router->get('/harvestCycle', 'cycle/index.php');
+$router->post('/harvestCycle/create', 'cycle/create.php');
+$router->get('/harvestCycle/edit', 'cycle/edit.php');
+$router->patch('/harvestCycle/patch', 'cycle/update.php');
+$router->delete('/harvestCycle/delete', 'cycle/destroy.php');
