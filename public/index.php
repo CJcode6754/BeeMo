2543c2c089a61ecb009692c715004561caac2959
CJcode6754 <ceejayibabiosa@gmail.com>
2025-05-15 19:19:24 +0800

    feat: add composer and add composer autoloading

--- public/index.php
+++ public/index.php
@@ -5,15 +5,14 @@ use Core\ValidationException;
 
 const BASE_PATH = __DIR__ . '/../';
 
+require BASE_PATH . 'vendor/autoload.php';
+
 session_start();
 
 require BASE_PATH . 'Core/function.php';
-spl_autoload_register(function ($class) {
-    // Core\DAtabase
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    require base_path("{$class}.php");
-});
+
 require base_path('bootstrap.php');
+
 $router =  new \Core\Router();
 
 $routes = require base_path('routes.php');
